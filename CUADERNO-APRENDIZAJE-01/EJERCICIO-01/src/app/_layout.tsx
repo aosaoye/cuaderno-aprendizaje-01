a67b0ff980462c8c3ac813d652aa2e1a0a06e1f5
Amalio OSA OYE <a29322@svalero.com>
2026-09-24 11:23:43 +0200

    fix: resolve Jetpack Compose Host boundary error in index.tsx

--- CUADERNO-APRENDIZAJE-01/EJERCICIO-01/src/app/_layout.tsx
+++ CUADERNO-APRENDIZAJE-01/EJERCICIO-01/src/app/_layout.tsx
@@ -4,11 +4,40 @@ import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
+import * as Font from 'expo-font';
+import {
+  Poppins_500Medium,
+  Poppins_400Regular,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  useFonts
+} from '@expo-google-fonts/dev';
+import { useEffect } from 'react';
+
 
 SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
+  const [fontLoaded, fontError] = useFonts({
+    Poppins_500Medium,
+    Poppins_400Regular,
+    Roboto_400Regular,
+    Roboto_500Medium,
+  });
+
+
+  useEffect(() => {
+    if(fontLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded, fontError])
+
+  if(!fontLoaded && !fontError) {
+    return null
+  }
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AnimatedSplashOverlay />
--- CUADERNO-APRENDIZAJE-01/EJERCICIO-01/src/app/index.tsx
+++ CUADERNO-APRENDIZAJE-01/EJERCICIO-01/src/app/index.tsx
@@ -2,12 +2,13 @@ import * as Device from 'expo-device';
 import { Platform, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { WebBadge } from '@/components/web-badge';
 import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
+import { Host, Button, BottomSheet } from '@expo/ui';
+import { useState } from 'react';
+import { Image } from 'expo-image';
 
 function getDevMenuHint() {
   if (Platform.OS === 'web') {
@@ -29,29 +30,32 @@ function getDevMenuHint() {
 }
 
 export default function HomeScreen() {
+  const [isPresented, setIsPresented] = useState(false);
+
+  
   return (
     <ThemedView style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+        <Host matchContents>
+          <Button label="Abrir Modal" onPress={() => setIsPresented(true)} />
+        </Host>
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
+        <BottomSheet isPresented={isPresented} onDismiss={() => setIsPresented(false)}>
+          <ThemedView style={styles.modalContent}>
+            <ThemedText type="subtitle">Modal</ThemedText>
+            <ThemedText>Desliza hacia arriba para expandir y hacia abajo para cerrar</ThemedText>
+            <Host matchContents>
+              <Button label="Cerrar" variant="outlined" onPress={() => setIsPresented(false)} />
+            </Host>
+          </ThemedView>
+        </BottomSheet>
 
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
+        <ThemedView style={styles.imageContainer}>
+          <Image
+            source={{
+              uri: 'https://thispersondoesnotexist.com/random-person.jpeg',
+            }}
+            style={styles.avatar}
           />
         </ThemedView>
 
@@ -94,5 +98,18 @@ const styles = StyleSheet.create({
     paddingHorizontal: Spacing.three,
     paddingVertical: Spacing.four,
     borderRadius: Spacing.four,
+  },
+  modalContent: {
+    gap: 12,
+    padding: Spacing.four,
+  },
+  imageContainer: {
+    marginTop: Spacing.four,
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
 });
